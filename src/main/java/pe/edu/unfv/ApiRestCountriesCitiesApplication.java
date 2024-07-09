4610a04a083de9b20578681250ded91630a2e4da
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-07-09 11:33:13 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/ApiRestCountriesCitiesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCountriesCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCountriesCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unfv/ApiRestCountriesCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCountriesCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
